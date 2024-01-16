9215e9b2ea169607e5fa2f77132e79bc31cd770c
Ajesh S <ajeshajesh9895@gmail.com>
2024-01-16 13:51:56 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,25 @@
+import './Footer.css'
+import Github from '../../assets/images/github.png'
+import Instagram from '../../assets/images/instagram.png'
+import LinkedIn from '../../assets/images/linkedin.png'
+import Logo from '../../assets/images/logo.png'
+const Footer = () => {
+  return (
+    <div className="Footer-container">
+      <hr />
+      <div className="footer">
+        <div className="social-links">
+          <img src={Github} alt="" />
+          <img src={Instagram} alt="" />
+          <img src={LinkedIn} alt="" />
+        </div>
+        <div className="logo-f">
+          <img src={Logo} alt="" />
+        </div>
+      </div>
+      <div className="blur blur-f-1"></div>
+      <div className="blur blur-f-2"></div>
+    </div>
+  )
+}
+export default Footer
---
+++ src/components/Programs/Programs.jsx
@@ -0,0 +1,29 @@
+import './Programs.css'
+import RightArrow from '../../assets/images/rightArrow.png'
+import { programsData } from '../../assets/data/programsData.jsx'
+const Programs = () => {
+  return (
+    <div className="Programs" id="programs">
+      {/* header */}
+      <div className="programs-header">
+        <span className="stroke-text">Explore our</span>
+        <span>Programs</span>
+        <span className="stroke-text">to shape you</span>
+      </div>
+      <div className="program-categories">
+        {programsData.map((program) => (
+          <div key={program.id} className="category">
+            {program.image}
+            <span>{program.heading}</span>
+            <span>{program.details}</span>
+            <div className="join-now">
+              <span>Join Now</span>
+              <img src={RightArrow} alt="" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+export default Programs
